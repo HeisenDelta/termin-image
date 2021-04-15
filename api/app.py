ab18a3f82c3bec1f5793339bfb2cbbba66cd1358
Heisen <ariya8.narayan7@outlook.jp>
2021-04-15 21:41:46 +0900

    Made details function, work later

--- api/app.py
+++ api/app.py
@@ -23,55 +23,55 @@ except (ModuleNotFoundError, ImportError):
 app = Flask(__name__)
 
 @app.route('/')
-def main(): return jsonify({ 'output': 'Head over to /api/ to get the strings' }, indent = 4)
+def main(): return jsonify({ 'output': 'Head over to /api/ to get the strings' })
 
 @app.route('/main/env/', methods = ['GET', 'POST'])
 def route_main_env():
     if request.method == 'GET':
-        path = request.args.get('path', None)       # Of the env file
+        env_path = request.args.get('env_path', None)           # Path to env file
 
         # Added error handling if path is not defined
         try: 
             return jsonify({ 
-                'output': function_main(path) 
-            }, indent = 4)
+                'output': function_main(env_path) 
+            })
 
         except FileNotFoundError:
             return jsonify({ 
-                'error': f'Path [{path}] is not defined' 
-            }, indent = 4)
+                'error': f'Path [{env_path}] is not defined' 
+            })
 
 
 @app.route('/main/mnl/', methods = ['GET', 'POST'])
 def route_main_mnl():
     if request.method == 'GET':
-        path = request.args.get('path', None)       # Of the image
+        img_path = request.args.get('img_path', None)           # Path to image file
         color = request.args.get('color', None)
         factor = request.args.get('factor', None)
 
         return jsonify({ 
-            'output': function_main_api(path, color, float(factor)) 
-        }, indent = 4)
+            'output': function_main_api(img_path, color, float(factor)) 
+        })
 
 
 @app.route('/profile/env/', methods = ['GET', 'POST'])
 def route_profile_env():
     if request.method == 'GET':
-        path = request.args.get('path', None)       # Of the env file
+        env_path = request.args.get('env_path', None)           # Path to env file
 
-        load_dotenv(dotenv_path = Path(path))
+        load_dotenv(dotenv_path = Path(env_path))
         X_OFFSET = os.getenv('X_OFFSET', None)
         Y_OFFSET = os.getenv('Y_OFFSET', None)
 
         return jsonify({ 
-            'output': function_profile_api(path, int(X_OFFSET), int(Y_OFFSET), same_path = False) 
-        }, indent = 4)
+            'output': function_profile_api(env_path, int(X_OFFSET), int(Y_OFFSET), same_path = False) 
+        })
 
 
 @app.route('/profile/mnl/', methods = ['GET', 'POST'])
 def route_profile_mnl():
     if request.method == 'GET': 
-        path = request.args.get('path', None)       # Of the image
+        img_path = request.args.get('img_path', None)           # Path to image file
         x_offset = request.args.get('x', None)
         y_offset = request.args.get('y', None)
         color = request.args.get('color', None)
@@ -79,9 +79,17 @@ def route_profile_mnl():
 
         return jsonify({ 
             'output': function_profile_api(
-                path, int(x_offset), int(y_offset), same_path = True, COLOR = color, FACTOR = float(factor)
+                img_path, int(x_offset), int(y_offset), same_path = True, COLOR = color, FACTOR = float(factor)
             ) 
-        }, indent = 4)
+        })
+
+
+@app.route('/details/env/', methods = ['GET', 'POST'])
+def route_details_mnl():
+    if request.method == 'GET':
+        env_path = request.args.get('env_path', None)           # Path to env file
+        det_path = request.args.get('det_path', None)           # Path to details file
+
 
 # Error handling exceptions
 @app.errorhandler(HTTPException)
--- library/details.py
+++ library/details.py
@@ -5,38 +5,47 @@ from pathlib import Path
 from rich.console import Console
 console = Console()
 
-from profile import ProfileImage
+from profile import ProfileImage, function_profile_api
 from main import load_env_file
 
-PATH_, COLOR, FACTOR = load_env_file()
+def function_details(det_file, env_path, mnl = True, x_offset = 0, y_offset = 0, color = 'True', factor = 1):
 
-image = ProfileImage(PATH_, float(FACTOR))
-image_text = image.select_profile(False).split('\n')
+    PATH_, COLOR, FACTOR = load_env_file(env_path = env_path)
 
-# Operations
+    image = ProfileImage(PATH_, float(FACTOR))
+    if mnl: image_text = image.select_profile(False).split('\n')
+    else:
+        image_text = function_profile_api(env_path, x_offset, y_offset, False, COLOR = color, FACTOR = float(factor)).split('\n')
 
-details = []
-with open('details.txt', 'r') as handle: 
-    details = ['\t\t' + line_.replace('\n', '') for line_ in handle.readlines() if line_.strip()]
 
-os.system('clear')
+    # Operations
 
-if len(details) > len(image_text): exit()                                                       # Currently exits if details exceed image text
+    details = []
+    with open(det_file, 'r') as handle: 
+        details = ['\t\t' + line_.replace('\n', '') for line_ in handle.readlines() if line_.strip()]
 
-if len(details) <= (len(image_text) // 2) - 1: 
-    details_ = []
-    for detail in details: 
-        details_.append(detail)
-        details_.append('')
-    details = details_[:len(details_) - 1]
+    if len(details) > len(image_text): details = details[:len(image_text)]          # Read only first few details if too many provided
 
-sps_length = (len(image_text) - len(details)) // 2
+    if len(details) <= (len(image_text) // 2) - 1: 
+        details_ = []
+        for detail in details: 
+            details_.append(detail)
+            details_.append('')
+        details = details_[:len(details_) - 1]
 
-for j in range(sps_length): console.print(image_text[j])
+    sps_length = (len(image_text) - len(details)) // 2
 
-for i in range(sps_length, len(image_text) - sps_length):
+    ret_str = ''
+    for j in range(sps_length): ret_str += image_text[j] + '\n'
+    for i in range(sps_length, len(image_text) - sps_length):
 
-    try: console.print(image_text[i] + details[i - sps_length])
-    except IndexError: console.print(image_text[i])
+        try: ret_str += image_text[i] + details[i - sps_length] + '\n'
+        except IndexError: ret_str += image_text[i] + '\n'
 
-for k in range(len(image_text) - sps_length, len(image_text)): console.print(image_text[k])
+    for k in range(len(image_text) - sps_length, len(image_text)): ret_str += image_text[k] + '\n'
+
+    return ret_str
+
+if __name__ == '__main__':
+    os.system('clear')
+    console.print(function_details(det_file = 'details.txt', env_path = 'PARAM.env'))
