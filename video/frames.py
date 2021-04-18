3948d7e7a5c4791930403c343b4c406690eae107
Heisen <ariya8.narayan7@outlook.jp>
2021-04-18 20:03:12 +0900

    Made image rendering numba

--- video/frames.py
+++ video/frames.py
@@ -4,6 +4,7 @@ import sys
 import time
 from PIL import Image
 from rich.console import Console
+from numba import jit
 console = Console()
 
 try: 
@@ -17,34 +18,33 @@ except (ModuleNotFoundError, ImportError):
     from main import TerminalImage
 
 video_ = cv2.VideoCapture('/home/heisendelta/Videos/dont_judge_me.mp4')
+term_size = (os.get_terminal_size().columns, os.get_terminal_size().lines)
+fps = 20
+
+@jit(nopython = True)                                   # Function for getting the dimensions
+def get_dim(factor, img_size, width):  
+
+    hbyw = img_size[1] / img_size[0]
+    wbyh = img_size[0] / img_size[1]
+
+    if width: return (int(term_size[0] * factor), round(term_size[0] * hbyw * 0.55 * factor))
+    else: return (round(term_size[1] * wbyh * factor / 0.55), int(term_size[1] * factor))
 
 
 def render_image_color(image, orientation = ['HEIGHT', 'WIDTH'], factor = 1.0):
 
-    start_time = time.time()
-
     imgSize = (image.shape[1], image.shape[0])
-    termSize = os.get_terminal_size()
-
-    # Image resizing
-
-    hbyw = imgSize[1] / imgSize[0]
-    wbyh = imgSize[0] / imgSize[1]
 
     if orientation == 'WIDTH':
-        image = cv2.resize(
-            image, (int(termSize.columns * factor), round(termSize.columns * hbyw * 0.55 * factor)), 
-            interpolation = cv2.INTER_AREA)   # Scale with width
+        image = cv2.resize( image, get_dim(factor, imgSize, True), interpolation = cv2.INTER_AREA )         # Scale with width
     elif orientation == 'HEIGHT':
-        image = cv2.resize(
-            image, (round(termSize.lines * wbyh * factor / 0.55), int(termSize.lines * factor)),
-            interpolation = cv2.INTER_AREA)       # Scale by height
+        image = cv2.resize( image, get_dim(factor, imgSize, False), interpolation = cv2.INTER_AREA )        # Scale by height
 
     imgSize = (image.shape[1], image.shape[0])
 
     str_ = ''
     for i in range(min(imgSize[1], 500)):
-        for j in range(min(imgSize[0], termSize.columns)):
+        for j in range(min(imgSize[0], term_size[0])):
 
             pix = image[i][j]
             hexCode = '#%02x%02x%02x' % (pix[2], pix[1], pix[0])
@@ -72,17 +72,17 @@ def get_frame(sec):
 if __name__ == '__main__':
 
     sec = 10
-    frame_rate = 1 / 20
-    count = 1
-
+    frame_rate = 1 / fps
     success = get_frame(sec)
 
     while success:
 
         try:
-            count += 1
-            sec += frame_rate
-            sec = round(sec, 2)
+            sec += round(frame_rate, 2)
+            start_time = time.time()
+
             success = get_frame(sec)
+
+            with open('times.txt', 'a') as handle: handle.writelines(str(time.time() - start_time) + '\n')
 
         except KeyboardInterrupt: break
