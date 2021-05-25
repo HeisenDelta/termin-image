c9eed4d9abbc8fca2158a911ac46e9e17c93f567
Heisen <ariya8.narayan7@outlook.jp>
2021-05-25 18:04:40 +0900

    Fixed grayscale image part

--- cpp/grayscale_image/scripts/grayscale.cpp
+++ cpp/grayscale_image/scripts/grayscale.cpp
@@ -3,6 +3,6 @@
 int main( void ) {
 
     Mat image = imread("/home/heisendelta/Pictures/Avalon.png", IMREAD_COLOR);
-    gcvimg::render_image_grayscale(image, true, false);
+    gcvimg::render_image_grayscale(image, true, true);
 
 }
